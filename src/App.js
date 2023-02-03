fa15383f3f76e905384662fdacf02cb928555b8e
geunee20 <geunee20@gmail.com>
2023-02-02 20:24:20 -0500

    .

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 const App = () => {
   useEffect(() => {
     // const APP_ID = "ebooth-gmknu";
-    const app = new Realm.App({ id: process.env.NODE_APP_ID });
+    const app = new Realm.App({ id: process.env.REACT_APP_ID });
     //Grab Tokens
     const params = new URLSearchParams(window.location.search);
     const token = params.get("token");
