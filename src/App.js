556cfc3d3df22de33dd39a0c73cc0b4d09aea883
geunee20 <geunee20@gmail.com>
2023-02-02 20:06:23 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import * as Realm from "realm-web";
+import { useEffect } from "react";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    // Load the environment variables
+    const app = new Realm.App({ id: "ebooth-gmknu" });
+    // Grab Tokens
+    const params = new URLSearchParams(window.location.search);
+    const token = params.get("token");
+    const tokenId = params.get("tokenId");
+    // Confirm client
+    app.emailPasswordAuth
+      .confirmUser({ token, tokenId })
+      .then(() => displayResult("success"))
+      .catch((err) => displayResult("error", err));
+    // Display Message depending on result
+    function displayResult(result, err) {
+      const message = document.getElementById("message");
+      if (result === "success") {
+        message.innerText =
+          "Your E-mail address has been verified.\n\n You may close this page. Thank you.";
+      } else if (result === "error") {
+        message.innerText =
+          "Unable to register this user. Please try again to register." + err;
+      }
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1 style={{ textAlign: "center" }}>eBooth</h1>
+        <h3 style={{ textAlign: "center" }}>Email Verification</h3>
+        <p
+          id="message"
+          style={{ textAlign: "center", backgroundColor: "whitesmoke" }}
+        ></p>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
