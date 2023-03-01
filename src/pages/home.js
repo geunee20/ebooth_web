f43e16de1681f08bf5d11aea5571cdb3ac225b9b
geunee20 <geunee20@gmail.com>
2023-03-01 02:31:41 -0500

    update ui

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import "./styles.css";
 import Wave from "react-wavify";
 import Typewriter from "typewriter-effect";
-import { UserState } from "realm-web";
 import { Link } from "react-router-dom";
 
 const HomeScreen = () => {
