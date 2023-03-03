295fa18d67a0ab06eafed1d057c252f6a608cca3
Shin, Yeajin <69772219+yshin8910@users.noreply.github.com>
2023-03-02 20:14:45 -0500

    Add terms_of_use.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import HomeScreen from "./pages/home";
 import PasswordResetPage from "./pages/password_reset";
 import EmailConfirmationPage from "./pages/email_confirmation";
 import PrivacyPolicyScreen from "./pages/privacy_policy";
+import TermsOfUseScreen from "./pages/terms_of_use";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/email-confirmation" element={<EmailConfirmationPage />} />
         <Route path="/password-reset" element={<PasswordResetPage />} />
         <Route path="/privacy-policy" element={<PrivacyPolicyScreen />} />
+        <Route path="/terms-of-use" element={<TermsOfUseScreen />} />
       </Routes>
     </Router>
   );
